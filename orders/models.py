ae9a8cefd18f539d7456fe7b63aec64fabe38179
Abai13 <abai20005shh@gmail.com>
2022-07-12 23:58:48 +0600

    orders, filter, search

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from products.models import Product
+
+
+User = get_user_model()
+
+STATUS_CHOICE = (
+    ('open', 'Открытый'),
+    ('in_process', 'В обработке'),
+    ('canceled', 'Отмененный'),
+    ('finished', 'Завершенный')
+)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='orders')
+    create_at = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=150, blank=True)
+    productcs = models.ManyToManyField(Product, through='OrderItems')
+    total_sum = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='open')
+
+    def __str__(self) -> str:
+        return f'Order #{self.id}'
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.RESTRICT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+    quantity = models.PositiveSmallIntegerField(default=0)
