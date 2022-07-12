a1c4896e3b8125e39770318d187c559c83378bff
Abai13 <abai20005shh@gmail.com>
2022-07-13 00:02:11 +0600

    orders, filter, search

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from products.filters import ProductPriceFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from .models import Product
 from .serializers import ProductSerializer, ProductListSerializer
@@ -8,6 +11,9 @@ from .serializers import ProductSerializer, ProductListSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
+    filterset_class = ProductPriceFilter
 
     def get_serializer_class(self):
         if self.action == 'list':
