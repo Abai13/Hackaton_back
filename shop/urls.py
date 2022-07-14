1847faac95f3eadf5289677b6f05139e78fafd12
Abai13 <abai20005shh@gmail.com>
2022-07-14 16:14:18 +0600

    image

--- shop/urls.py
+++ shop/urls.py
@@ -41,8 +41,7 @@ urlpatterns = [
     path('swagger/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
