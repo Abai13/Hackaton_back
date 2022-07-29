16609974dbac59906971bad3c12d0fb3da76910d
Abai13 <105208558+Abai13@users.noreply.github.com>
2022-07-29 15:11:21 +0600

    Update config.py

--- telegram/config.py
+++ telegram/config.py
@@ -1,2 +1,2 @@
-open_weather_token = 'f35bf661d2608932f8e835370e809767'
-tg_bot_token = '5513518320:AAEePKFaPJCLWL35P3z0QzIXLwqpzxtlnBE'
+open_weather_token = 'open weather api'
+tg_bot_token = 'tg bot token'
